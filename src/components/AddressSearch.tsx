7442f118763e3af3d40c745db1a8eccf18907469
Lucas Fiorelli <fiorellilucas@gmail.com>
2025-05-19 20:09:08 -0300

    Buscar endereço pelo nome em vez do endereço

--- src/components/AddressSearch.tsx
+++ src/components/AddressSearch.tsx
@@ -29,7 +29,7 @@ const AddressSearch: React.FC<AddressSearchProps> = ({ onAddressSelect }) => {
       .then(json => {
         let address_array: string[] = []
         for (let idx in json) {
-          address_array.push(`${json[idx]["endereco"]} - ${json[idx]["cidade"]}/${json[idx]["estado"]}`)
+          address_array.push(`${json[idx]["nome_estabelecimento"]} - ${json[idx]["cidade"]}/${json[idx]["estado"]}`)
         }
         setAddresses(address_array)
       })
@@ -62,7 +62,7 @@ const AddressSearch: React.FC<AddressSearchProps> = ({ onAddressSelect }) => {
           type="text"
           value={searchTerm}
           onChange={(e) => handleSearch(e.target.value)}
-          placeholder="Digite o endereço ou estabelecimento"
+          placeholder="Digite o nome do estabelecimento"
           className="w-full p-3 pl-10 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
         <Search className="absolute left-3 top-3.5 h-5 w-5 text-gray-400" />
